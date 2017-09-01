35c2d23a41094e56a69e5ffd6ddc807120187c83
yitiaoluoyuo <1511182565@qq.com>
2017-09-01 10:07:03 +0800

    shop cart

---
+++ xiaoxu-ec/src/main/java/com/xiaoxu/xiaoxu_ec/main_delegates/mine/List/ListBean.java
@@ -0,0 +1,115 @@
+package com.xiaoxu.xiaoxu_ec.main_delegates.mine.List;
+
+import android.widget.CompoundButton;
+
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+import com.xiaoxu.xiaoxu_core.delegates.XiaoXuDelegate;
+
+/**
+ * Created by xiaoxu on 2017/9/1.
+ */
+
+public class ListBean implements MultiItemEntity {
+    private int mItemType = 0;
+    private String mImageUrl = null;
+    private String mText = null;
+    private String mValue = null;
+    private int mId = 0;
+    private XiaoXuDelegate mDelegate = null;
+    private CompoundButton.OnCheckedChangeListener mOnCheckedChangeListener = null;
+
+    public ListBean(int mItemType, String mImageUrl, String mText, String mValue, int mId, XiaoXuDelegate mDelegate, CompoundButton.OnCheckedChangeListener mOnCheckedChangeListener) {
+        this.mItemType = mItemType;
+        this.mImageUrl = mImageUrl;
+        this.mText = mText;
+        this.mValue = mValue;
+        this.mId = mId;
+        this.mDelegate = mDelegate;
+        this.mOnCheckedChangeListener = mOnCheckedChangeListener;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getText() {
+        if (mText == null) {
+            return "";
+        }
+        return mText;
+    }
+
+    public String getValue() {
+        if (mValue == null) {
+            return "";
+        }
+        return mValue;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public XiaoXuDelegate getDelegate() {
+        return mDelegate;
+    }
+
+    public CompoundButton.OnCheckedChangeListener getmOnCheckedChangeListener() {
+        return mOnCheckedChangeListener;
+    }
+
+    @Override
+    public int getItemType() {
+        return mItemType;
+    }
+
+    public static final class Builder {
+
+        private int id = 0;
+        private int itemType = 0;
+        private String imageUrl = null;
+        private String text = null;
+        private String value = null;
+        private CompoundButton.OnCheckedChangeListener onCheckedChangeListener = null;
+        private XiaoXuDelegate delegate = null;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setItemType(int itemType) {
+            this.itemType = itemType;
+            return this;
+        }
+
+        public Builder setImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+
+        public Builder setText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder setValue(String value) {
+            this.value = value;
+            return this;
+        }
+
+        public Builder setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener onCheckedChangeListener) {
+            this.onCheckedChangeListener = onCheckedChangeListener;
+            return this;
+        }
+
+        public Builder setDelegate(XiaoXuDelegate delegate) {
+            this.delegate = delegate;
+            return this;
+        }
+
+        public ListBean build() {
+            return new ListBean(itemType, imageUrl, text, value, id, delegate, onCheckedChangeListener);
+        }
+    }
+}
